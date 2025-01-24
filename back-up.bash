d413b9ebbf281d9f4b526671cbb29cd9a2f3539b
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2025-01-24 21:28:20 +0900

    back-up.bash 파일 작성

---
+++ back-up.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+DATE=$(date + "%Y-%m-%d")
+JENKINS_HOME="/home/ec2-user/jenkins_home"
+BACKUP_DIR="/home/ec2-user/backup"
+CONTAINER_NAME="jenkins"
+
+tar -czvf ${BACKUP_DIR}/jenkins_home_${DATE}.tar.gz ${JENKINS_HOME}
