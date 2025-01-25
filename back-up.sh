925465da10d85836a2ca1e9de523d2191de9b17e
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2025-01-25 21:32:37 +0900

    s3 전송 스크립트 추가

--- back-up.sh
+++ back-up.sh
@@ -3,5 +3,7 @@ DATE=$(date +"%Y-%m-%d")
 JENKINS_HOME="/home/ec2-user/jenkins_home"
 BACKUP_DIR="/home/ec2-user/backup"
 CONTAINER_NAME="jenkins"
+BACKUP_FILE = "${BACKUP_DIR}/jenkins_home_${DATE}.tar.gz"
 
-tar -czvf ${BACKUP_DIR}/jenkins_home_${DATE}.tar.gz ${JENKINS_HOME}
+tar -czvf ${BACKUP_FILE} ${JENKINS_HOME}
+aws s3 cp ${BACKUP_FILE} s3://earlybird-jenkins-backup
